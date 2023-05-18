606396f7299498e7883784c55132ede99089aa2c
Brian Rinaldi <brian.rinaldi@gmail.com>
2023-05-18 09:40:53 -0400

    Change to server rendered

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://toggle-portfolio.netlify.app/",
+  output: "server",
+  adapter: netlify()
 });
